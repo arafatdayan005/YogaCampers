418cc32fb6b1cae2f3682f35d22b85d72a58bb2f
arafatdayan005 <arafatdayan005@gmail.com>
2023-06-12 03:23:59 +0600

    dynamically rendering dashboard added

--- src/Pages/DashBoard/Sidebar.js
+++ src/Pages/DashBoard/Sidebar.js
@@ -4,12 +4,59 @@ import { Link } from 'react-router-dom'
 
 function Sidebar() {
     const [toggle, setToggle] = useState(false)
-    const { user, loader, logOut } = useContext(AuthContext)
+    const { user, loader, role, logOut } = useContext(AuthContext)
 
     if (loader) {
         <p>loading..</p>
     }
-    if (user) {
+
+    if (user && role) {
+        let userRole;
+
+        if (role === "admin") {
+            userRole =
+                <div>
+                    <li>
+                        <Link to="/dashboard/admin/classes" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-100 dark:hover:bg-gray-700">
+                            <span className="ml-3">Manage Classes</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/admin/users" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-100 dark:hover:bg-gray-700">
+                            <span className="ml-3">Manage Users</span>
+                        </Link>
+                    </li>
+                </div>
+        } else if (role === "instructor") {
+            userRole =
+                <div>
+                    <li>
+                        <Link to="/dashboard/instructor/myclasses" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-100 dark:hover:bg-gray-700">
+                            <span className="ml-3">My Classes</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/instructor/newclass" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-100 dark:hover:bg-gray-700">
+                            <span className="ml-3">Add a class</span>
+                        </Link>
+                    </li>
+                </div>
+        } else {
+            userRole =
+                <div>
+                    <li>
+                        <Link to="/dashboard/student/myclasses" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-100 dark:hover:bg-gray-700">
+                            <span className="ml-3">Selected Classes</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/student/enrolled" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-100 dark:hover:bg-gray-700">
+                            <span className="ml-3">Enrolled classes</span>
+                        </Link>
+                    </li>
+                </div>
+        }
+
         return (
             <>
                 <button onClick={() => setToggle(!toggle)} type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
@@ -25,19 +72,11 @@ function Sidebar() {
                         <div className='my-8 flex flex-col items-center'>
                             <img className='rounded-full' src={user?.photoURL} alt="" />
                             <p className='text-xl font-semibold mt-4'> {user?.displayName} </p>
+                            <p className='capitalize bg-emerald-400 text-white px-2 rounded-full mt-2'> {role} </p>
                         </div>
 
                         <ul className="space-y-2 font-medium">
-                            <li>
-                                <Link to="/dashboard/instructor/myclasses" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-100 dark:hover:bg-gray-700">
-                                    <span className="ml-3">My Classes</span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/dashboard/instructor/newclass" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-100 dark:hover:bg-gray-700">
-                                    <span className="ml-3">Add a class</span>
-                                </Link>
-                            </li>
+                            {userRole}
                             <div className='absolute bottom-10'>
                                 <Link to="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-green-100 dark:hover:bg-gray-700">
                                     <span className="ml-3 pr-24">Back to Home</span>
