12db2a9e8feeb1760260e1ea122d5a8f95985f18
Arafat <arafatdayan005@gmail.com>
2024-08-19 21:18:08 +0600

    Classes Page Redesign [DONE]

--- src/Pages/Classes/ClassCards.js
+++ src/Pages/Classes/ClassCards.js
@@ -1,38 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import { getSelectedClass } from '../../API/class'
+import React, { useEffect, useState } from "react";
+import { getSelectedClass } from "../../API/class";
 
 function ClassCards({ data, handleSelect, userEmail, role }) {
-    const [studentEmail, setStudentEmail] = useState(null)
-    const [reload, setReload] = useState(false)
-    const [disabled, setDisabled] = useState(false)
-    const { _id, image, name, instructor, description, price, seat, enrolled } = data
+  const [studentEmail, setStudentEmail] = useState(null);
+  const [reload, setReload] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const { _id, image, name, instructor, price, seat, enrolled } = data;
 
-    useEffect(() => {
-        getSelectedClass(data._id, userEmail)
-            .then(res => res.json())
-            .then(data => setStudentEmail(data.studentEmail))
-            .catch(err => console.log(err))
-    }, [reload, userEmail, data._id])
+  useEffect(() => {
+    getSelectedClass(data._id, userEmail)
+      .then((res) => res.json())
+      .then((data) => setStudentEmail(data.studentEmail))
+      .catch((err) => console.log(err));
+  }, [reload, userEmail, data._id]);
 
-    return (
-        <div className='shadow-lg rounded-xl bg-lime-100 dark:bg-teal-800'>
-            <img className='h-64 w-full rounded-xl  rounded-bl-none rounded-br-none' src={image} alt="" />
-            <div className='px-8 py-6 text-center'>
-                <p className='font-semibold text-3xl text-gray-600 dark:text-white'> {name} </p>
-                <p className='font-medium text-lg text-gray-600 dark:text-white mt-2'> Instructor: {instructor.name} </p>
-                <div className='flex justify-around mt-4'>
-                    <p className='text-lg text-gray-600 dark:text-white'> Available Seat: {seat - enrolled} </p>
-                    <p className='text-lg text-gray-600 dark:text-white'> Price: ${price} </p>
-                </div>
-                <p className='text-left my-4'> Details: {description.slice(0, 180)}... </p>
-                <button onClick={() => {
-                    handleSelect(_id, name, image, price, instructor)
-                    setReload(!reload)
-                    setDisabled(true)
-                }} className={`mx-auto bg-lime-500 ${userEmail === studentEmail || disabled ? "bg-lime-600" : "hover:bg-lime-600"} ${role === "instructor" ? "bg-lime-500 hover:bg-lime-500 cursor-not-allowed" : ""} px-6 py-2 rounded-full text-white`} disabled={userEmail === studentEmail || disabled || role === "instructor"}>{ (role && userEmail === studentEmail) || (role && disabled) ? "Selected" : "Select"}</button>
-            </div>
+  return (
+    <div
+      data-aos="zoom-in"
+      data-aos-delay="300"
+      data-aos-once
+      className="relative max-w-[370px] mx-auto"
+    >
+      <img className="h-64 w-full" src={image} alt="" />
+      <div className="absolute z-10 p-4 rounded-lg text-center bg-white dark:bg-emerald-900 w-[80%] left-0 right-0 mx-auto -bottom-[50%] shadow-[0_0_25px_0_rgba(35,31,32,0.1)] duration-500">
+        <p className="font-semibold text-2xl text-[#17543E] dark:text-white font-caudex">
+          {name}
+        </p>
+        <p className="font-medium text-lg text-gray-600 dark:text-white">
+          Trainer: {instructor.name}
+        </p>
+        <div className="flex justify-around my-2">
+          <p className="text-lg text-gray-600 dark:text-white">
+            Available Seat:{" "}
+            <span className="text-[#D6A944] font-semibold">
+              {seat - enrolled}
+            </span>
+          </p>
+          <p className="text-lg text-gray-600 dark:text-white">
+            Price:{" "}
+            <span className="text-[#D6A944] font-semibold">${price}</span>
+          </p>
         </div>
-    )
+        <button
+          onClick={() => {
+            handleSelect(_id, name, image, price, instructor);
+            setReload(!reload);
+            setDisabled(true);
+          }}
+          className={`mx-auto border-2 border-[#D6A944] ${
+            userEmail === studentEmail || disabled
+              ? "bg-[#D6A944] hover:text-white duration-500"
+              : "hover:bg-[#D6A944] hover:text-white duration-500"
+          } px-6 py-2 rounded-full text-[#D6A944] disabled:cursor-not-allowed`}
+          disabled={
+            userEmail === studentEmail || disabled || role === "instructor"
+          }
+        >
+          {(role && userEmail === studentEmail) || (role && disabled)
+            ? "Selected"
+            : "Select"}
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default ClassCards
+export default ClassCards;
--- src/Pages/Classes/Classes.js
+++ src/Pages/Classes/Classes.js
@@ -1,57 +1,95 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { getApprovedClasses, selectClass } from '../../API/class'
-import ClassCards from './ClassCards'
-import { AuthContext } from '../../Providers/AuthProviders'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import { getApprovedClasses, selectClass } from "../../API/class";
+import ClassCards from "./ClassCards";
+import { AuthContext } from "../../Providers/AuthProviders";
+import { useNavigate } from "react-router-dom";
 
 function Classes() {
-    const [classData, setClassData] = useState([])
-    const [reload, setReload] = useState(false)
-    const { user, role } = useContext(AuthContext)
-    const navigate = useNavigate()
+  const [classData, setClassData] = useState([]);
+  const [reload, setReload] = useState(false);
+  const { user, role } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        getApprovedClasses()
-            .then(data => {
-                setClassData(data)
-            })
-    }, [user, reload])
+  useEffect(() => {
+    getApprovedClasses().then((data) => {
+      setClassData(data);
+    });
+  }, [user, reload]);
 
-    const handleSelect = (id, name, image, price, instructor) => {
-        if (!role) {
-            navigate("/login")
-        } else {
-            const data = {
-                class_id: id,
-                className: name,
-                classImage: image,
-                price: price,
-                instructor: instructor,
-                studentName: user.displayName,
-                studentEmail: user.email,
-                enrolled: false
-            }
-
-            selectClass(data)
-            setReload(!reload)
-        }
-
-
+  const handleSelect = (id, name, image, price, instructor) => {
+    if (!role) {
+      navigate("/login");
+    } else {
+      const data = {
+        class_id: id,
+        className: name,
+        classImage: image,
+        price: price,
+        instructor: instructor,
+        studentName: user.displayName,
+        studentEmail: user.email,
+        enrolled: false,
+      };
+      selectClass(data);
+      setReload(!reload);
     }
+  };
 
-    return (
-        <div className='pt-20 md:w-[90%] mx-auto dark:text-white'>
-            <p className='text-4xl font-serif my-8 text-gray-600 dark:text-white'>Available Classes</p>
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-x-8 gap-y-8 pb-12'>
-                {
-                    classData.map(data =>
-                        <ClassCards key={data._id} data={data} handleSelect={handleSelect} userEmail={user?.email} role={role} ></ClassCards>
-                    )
-                }
-            </div>
+  return (
+    <div className="md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl min-h-screen mx-auto py-10 lg:py-16 px-8">
+      <h3
+        className="text-2xl  text-[#D6A944] text-center font-caudex"
+        data-aos="zoom-in"
+        data-aos-once
+      >
+        Classes
+      </h3>
+      <h1
+        className="mt-4 mb-6 text-4xl lg:text-5xl text-[#17543E] dark:text-white duration-500 font-semibold font-caudex text-center"
+        data-aos="zoom-in"
+        data-aos-delay="300"
+        data-aos-once
+      >
+        Our Special Trainings
+      </h1>
 
+      {classData.length === 0 ? (
+        <div className="flex justify-center items-center text-center h-[50vh]">
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="inline w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-green-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
         </div>
-    )
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-40 pt-12 pb-24">
+          {classData.map((data) => (
+            <ClassCards
+              key={data._id}
+              data={data}
+              handleSelect={handleSelect}
+              userEmail={user?.email}
+              role={role}
+            ></ClassCards>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Classes
+export default Classes;
